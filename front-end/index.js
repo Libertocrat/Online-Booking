37a036d9c971927644405ccaddd3d5644fb1cd73
Libertocrat <libertocrat@gmail.com>
2022-11-02 19:15:07 -0600

    Add: SASS/SCSS Compiler & Loader

--- front-end/index.js
+++ front-end/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-//import './sass/main.scss';
-import './styles.css';
+import './sass/main.scss';
+import './styles.css'; //Just for testing, it may be removed if working just with SASS/SCSS
 
 ReactDOM.render(
-  <h1>Hello, React & Django!</h1>,
+  <h1>Hello React, Sass & Django!</h1>,
   document.getElementById('root')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -6,17 +6,25 @@ module.exports = {
     filename: 'index-bundle.js',  // output bundle file name
     path: path.resolve(__dirname, './static'),  // path to our Django static directory
   },
-  module: { // React files loader with Babel
+  module: { 
     rules: [
-      {
+      { // React files loader with Babel
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: "babel-loader",
         options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
       },
-      {
+      { // CSS loaders
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader"]
+      },
+      { // Sass & SCSS loaders
+        test: /\.s[ac]ss$/i,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+        ]
       },
     ]
   }
