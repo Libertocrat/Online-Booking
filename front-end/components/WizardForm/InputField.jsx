bc81abc9df73e0baf8efecf0b3c67b28199a9058
Libertocrat <libertocrat@gmail.com>
2022-12-08 16:10:25 -0600

    Edit: InputField controlled component

--- front-end/components/WizardForm/InputField.jsx
+++ front-end/components/WizardForm/InputField.jsx
@@ -3,9 +3,31 @@ import styles from "./WizardForm.module.scss";
 
 function InputField(props) {
 
+    const [state,setState] = useState(
+        {
+            input: ''
+        }
+    );
+
+    function onChangeHandler(event) {
+
+        setState((prevState) => {
+            return {
+            ...prevState,
+            input: event.target.value
+            };
+        });
+    }
+
     return(
         <React.Fragment>
-            <input type={props.type} name={props.name}  placeholder={props.placeholder} />
+            <input 
+                type={props.type} 
+                name={props.name}  
+                placeholder={props.placeholder} 
+                value={state.input}
+                onChange={onChangeHandler}/>
+            <p>{state.input}</p>
         </React.Fragment>
     );
 }
